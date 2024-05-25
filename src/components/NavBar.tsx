3017a53b3d624e7bfa55c7bf9f0435e9c37bdf65
GrzegorzKwiatkowski <117453707+GrzegorzKwiatkowski@users.noreply.github.com>
2024-05-26 01:07:57 +0200

    Implement searching

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,12 +2,15 @@ import { HStack, Image, Text } from "@chakra-ui/react";
 import logo from "../assets/GameHub_logo.png";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchImput from "./SearchImput";
+interface Props {
+  onSearch: (searchText: string) => void;
+}
 
-const NavBar = () => {
+const NavBar = ({ onSearch }: Props) => {
   return (
     <HStack padding={"10px"}>
       <Image src={logo} boxSize={"60px"}></Image>
-      <SearchImput></SearchImput>
+      <SearchImput onSearch={onSearch}></SearchImput>
       <ColorModeSwitch />
     </HStack>
   );
