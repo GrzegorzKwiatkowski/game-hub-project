75c3992f459dedd846853bd7987b05929a81cd36
GrzegorzKwiatkowski <117453707+GrzegorzKwiatkowski@users.noreply.github.com>
2024-05-26 22:14:03 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./GrzegorzKwiatkowski/game-hub-project",
+  base: "./https://github.com/GrzegorzKwiatkowski/game-hub-project",
   plugins: [react()],
 })
