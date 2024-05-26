46f19f5ad62c5ad2645d460e488f24fc66f88b06
GrzegorzKwiatkowski <117453707+GrzegorzKwiatkowski@users.noreply.github.com>
2024-05-26 22:35:35 +0200

    add:deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./https://github.com/GrzegorzKwiatkowski/game-hub-project",
+  base: "/game-hub-project/",
   plugins: [react()],
 })
